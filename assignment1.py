a1941bcb62e80fecb50d24b40160eb14054acd0f
jwinton8 <jessica.winton@my.jcu.edu.au>
2016-04-01 02:11:40 +1000

    Initial commit

---
+++ assignment1.py
@@ -0,0 +1,14 @@
+# CP 1404 Assesment 1 - 2016
+# Items for Hire
+# Jessica Winton, March 31, 2016
+
+print("Items for Hire - Jessica Winton")
+
+
+def main():
+    print("Items for Hire!")
+    print("Written by Jessica Winton, March 2016")
+
+    print("MENU")
+    choice = input(">>> ").upper()
+    while choice !=  "L":
